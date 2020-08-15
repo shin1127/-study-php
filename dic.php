a97fdc3429fdb23f8d0d98bff14f75c36f84f274
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-08-15 21:55:21 +0900

    Create dic.php

---
+++ dic.php
@@ -0,0 +1,20 @@
+<?php
+// Your code here!
+
+// 配列
+
+$ary = array("apple", "microsoft", "facebook");
+$dic = array("orange"=>"orange", "apple"=>"red", "grape"=>"purple");
+
+
+echo $ary[0];
+echo ("\n");
+echo $dic["apple"];
+
+
+
+
+
+?>
+
+// https://creive.me/archives/8719/
