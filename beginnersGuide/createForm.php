433aff0db3de0bef79afb08bf9e52ccfbd456742
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-08-18 15:24:46 +0900

    Add files via upload

---
+++ beginnersGuide/createForm.php
@@ -0,0 +1,25 @@
+<?php
+    if (isset($_POST['text1'])){
+        $input = $_POST['text1'];
+        $result = "あなたは、「{$input}」と書きました。";
+    }else{
+        $result = "なにか書いてください。";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <div><?php echo $result; ?></div>
+    <form method="post" action="./createForm.php">
+    <input type="text" name="text1">
+    <input type="submit">
+    </form>
+</body>
+</html>
