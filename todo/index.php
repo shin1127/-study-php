fd73d5a0bb0a7430fb7982b573b4c78c8334a95c
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-08-26 12:34:39 +0900

    Add files via upload

---
+++ todo/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todoリスト</title>
+</head>
+<body>
+    <h1>Todoリスト</h1>
+    <form action="index.php" method="post">
+        <ul>
+            <li><span>タスク名</span>
+        </li>
+        <li>
+        <input type="submit" name="submit">
+        </li>
+        </ul>
+    </form>
+    
+</body>
+</html>
