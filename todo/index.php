bbcc616c436afbbbb091ea47116fbdf1db8675f7
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-08-28 01:03:02 +0900

    Update index.php

--- todo/index.php
+++ todo/index.php
@@ -1,34 +1,44 @@
 <?php
+
+// クラスの中身はどうやって調べるんだ？
+
 // echo "<pre>";
 // var_dump($_POST);
 // echo "</pre>";
 
-if(isset($_POST["submit"])){
+if(isset($_POST["submit"])){  // $_POSTにsubmitが存在するか？
 
     $name = $_POST["name"];
-    $name = htmlspecialchars($name, ENT_QUOTES);
+    $name = htmlspecialchars($name, ENT_QUOTES);  // SQLインジェクション対策の関数
 
     $dsn =
     "mysql:dbname=todolist;host=localhost;charset=utf8";
     $user = "root";
     $password = "1234";
 
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->query("SET NAMES utf8");
+    $dbh = new PDO($dsn, $user, $password);  // PDO: DB接続をするためのクラス
+    $dbh->query("SET NAMES utf8");  // DB接続時の文字化けを解消する, -> はプロパティへのアクセス、メソッドの呼び出し
 
-    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);  // https://www.php.net/manual/ja/pdo.setattribute.php
 
-    $sql = "insert into tasks (name, done) values (?, 0)";
+    $sql = "insert into tasks (name, done) values (?, 0)";  // SQLインジェクション対策のプレースホルダ(=?)
     $stmt = $dbh->prepare($sql);
 
-    $stmt->bindValue(1, $name, PDO::PARAM_STR);
+    $stmt->bindValue(1, $name, PDO::PARAM_STR);  // たぶん$sqlの第一引数"name"に"$name"が対応してる SQLに値をバインド（挿入？）する
+
+    // PDO::PARAM_STRについて http://blog.a-way-out.net/blog/2013/12/18/pdo-prepare-statement-numeric-literal-part2/
 
     $stmt->execute();
 
     $dbh = null;
 
     unset($name);
+
+    
 }
+// echo($dbh);
+// get_class_vars(string $dbh):array;
+var_dump($dbh);
 
 
 ?>
