a9ab3bb965ae75dd7932c699099044ddbf8e9d95
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-08-27 13:45:38 +0900

    DB接続

--- todo/index.php
+++ todo/index.php
@@ -1,3 +1,38 @@
+<?php
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
+if(isset($_POST["submit"])){
+
+    $name = $_POST["name"];
+    $name = htmlspecialchars($name, ENT_QUOTES);
+
+    $dsn =
+    "mysql:dbname=todolist;host=localhost;charset=utf8";
+    $user = "root";
+    $password = "1234";
+
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query("SET NAMES utf8");
+
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $sql = "insert into tasks (name, done) values (?, 0)";
+    $stmt = $dbh->prepare($sql);
+
+    $stmt->bindValue(1, $name, PDO::PARAM_STR);
+
+    $stmt->execute();
+
+    $dbh = null;
+
+    unset($name);
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +44,7 @@
     <h1>Todoリスト</h1>
     <form action="index.php" method="post">
         <ul>
-            <li><span>タスク名</span>
+            <li><span>タスク名</span><input type="text" name="name">
         </li>
         <li>
         <input type="submit" name="submit">
